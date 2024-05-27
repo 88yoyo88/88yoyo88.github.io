f556f0083d79560f768f2b68ae1d215104861bee
88yoyo88 <157976667+88yoyo88@users.noreply.github.com>
2024-05-27 17:59:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 //gsap.to('.logo', {y: 300, duration: 2})
+
+ window.addEventListener('scroll', () => {
+        document.body.style.setProperty('--scroll', window.scrollY / (document.body.offsetHeight - window.innerHeight))
+        })
